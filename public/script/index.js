1491447884c8e7b0dfe7668d5979b5cc5bca2b0b
juniorbonini <juniorboninia7x@gmail.com>
2026-05-12 20:39:56 -0300

    feat(js): cria classe principal do parquimetro

---
+++ public/script/index.js
@@ -0,0 +1,11 @@
+class Parquimetro {
+  constructor() {
+    this.tableValues = [
+      { value: 1.0, time: 30 },
+      { value: 1.75, time: 60 },
+      { value: 3.0, time: 120 },
+    ];
+  }
+
+
+}
